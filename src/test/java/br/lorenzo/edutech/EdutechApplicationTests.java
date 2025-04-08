1c449cc2da154c976fa1b442677322a8bd42e11e
lorenzo-nascimento <lorenzonascimento.dev@gmail.com>
2025-04-08 16:28:22 -0300

    chore: inicia projeto Spring Boot para gestão de cursos online

---
+++ src/test/java/br/lorenzo/edutech/EdutechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.lorenzo.edutech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdutechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
